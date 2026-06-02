bdb3f5bde3063c2884438d7c9acb668eac10957a
JVGirardi <joaovgirardi@gmail.com>
2026-06-01 22:43:44 -0300

    Requisitos nao funcionais, metodo decorator --> logar tempo de execucao

---
+++ typescript-curso-modulo-3/app/src/decorators/logar-tempo-de-execucao.ts
@@ -0,0 +1,21 @@
+export function logarTempoDeExecucao() {
+    return function(
+        target: any, //se colocar decorator em um metodo estático target é a funcao construtora da classe. se n for estático retorna o prototype da classe, prototype = 'método herdado'
+        propertyKey: string, //nome do método como string que foi decorado
+        descriptor: PropertyDescriptor //referencia para o método original
+    ) {
+        const metodoOriginal = descriptor.value; //function vai receber uma quantidade indefinidade de parametros
+        descriptor.value = function(...args: Array<any> ) {//ou any[]--> ...args pega os parametros passados e converte para um array
+            const t1 = performance.now();
+
+            const retorno = metodoOriginal.apply(this, args); //chama o metodo original this vai ser a classe filha onde sera executada,
+                                                              //passo como parametro o array de parametros
+
+            const t2 = performance.now();
+            console.log(`${propertyKey}, tempo de execucao ${t2-t1/1000} segundos`);
+            retorno
+        };
+
+        return descriptor;
+    }
+}
