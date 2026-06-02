03f0465b1d94e8362b1f1cc6c08fdcbc2fac5016
JVGirardi <joaovgirardi@gmail.com>
2026-06-01 23:45:17 -0300

    Criando decorator escape e inspect

--- typescript-curso-modulo-3/app/src/decorators/logar-tempo-de-execucao.ts
+++ typescript-curso-modulo-3/app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,4 +1,4 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos: boolean = false) { //default false
     return function(
         target: any, //se colocar decorator em um metodo estático target é a funcao construtora da classe. se n for estático retorna o prototype da classe, prototype = 'método herdado'
         propertyKey: string, //nome do método como string que foi decorado
@@ -6,13 +6,17 @@ export function logarTempoDeExecucao() {
     ) {
         const metodoOriginal = descriptor.value; //function vai receber uma quantidade indefinidade de parametros
         descriptor.value = function(...args: Array<any> ) {//ou any[]--> ...args pega os parametros passados e converte para um array
+            let divisor = 1;
+            let unidade = 'milisegundos';
+            if (emSegundos) {
+                divisor = 1000;
+                unidade = 'segundos';
+            }
             const t1 = performance.now();
-
             const retorno = metodoOriginal.apply(this, args); //chama o metodo original this vai ser a classe filha onde sera executada,
                                                               //passo como parametro o array de parametros
-
             const t2 = performance.now();
-            console.log(`${propertyKey}, tempo de execucao ${t2-t1/1000} segundos`);
+            console.log(`${propertyKey}, tempo de execucao ${t2-t1/divisor} ${unidade}`);
             retorno
         };
 
